8e71fbbdda5e58035e84121450b213a80bae5056
satyendra <msatyendra567@gmail.com>
2024-09-10 15:28:40 +0530

    side bar added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import GraphCanvas from './components/GraphCanvas'
 import FlowChart from './components/FlowChart'
+import Sidebar from './components/Sidebar'
 
 const App = () => {
   return (
     <>
     
     {/* <GraphCanvas/> */}
-
+    <Sidebar />
     <FlowChart/>
     </>
   )
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { FiLogOut } from 'react-icons/fi'; 
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoTimer } from "react-icons/io5";
+import { BsStack } from "react-icons/bs";
+import { FaGitAlt } from "react-icons/fa";
+import './Sidebar.css';
+import logo from '../assets/logo.png'
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      {/* Logo Section */}
+      <div>
+
+        <div className="sidebar-logo">
+            <img src={logo} alt="Logo" />
+        </div>
+
+        {/* Icons Section */}
+        <div className="sidebar-icons">
+            <GiHamburgerMenu size={24} color="#fff" />
+            <IoTimer size={24} color="#fff" />
+            <BsStack size={24} color="#fff" />
+            <FaGitAlt size={24} color="#fff" />
+        </div>
+      </div>
+
+      {/* Avatar Section */}
+      <div className="sidebar-avatar">
+        <img
+          src="https://www.pngarts.com/files/5/User-Avatar-PNG-Transparent-Image.png"
+          alt="User Avatar"
+          className="avatar"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
